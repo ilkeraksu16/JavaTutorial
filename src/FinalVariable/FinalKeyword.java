ccdbad34f4b37cc661c913078d5605c33082828f
İlker Aksu <ilkeraksu16@users.noreply.github.com>
2022-04-14 09:26:52 +0300

    değişkenler üzerinde final keyword kullanımı

---
+++ src/FinalVariable/FinalKeyword.java
@@ -0,0 +1,23 @@
+package FinalVariable;
+
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int i = 8;
+		System.out.println(i);
+		 // hata
+		 //i= 7;
+		// Following is not illegal
+		final long l; 
+		System.out.println(1);
+		
+		System.out.println(1);
+		
+		int x = 3;
+		int y = 8;
+		final int j;
+	}
+
+}
