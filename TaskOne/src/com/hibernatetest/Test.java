a9cb7ccfc0d2ecac0bc86799088f4e2aa8b8c2f4
İlker Aksu <ilkeraksu16@users.noreply.github.com>
2022-04-25 10:58:35 +0300

    hibernate jarlarıeklendi

---
+++ TaskOne/src/com/hibernatetest/Test.java
@@ -0,0 +1,13 @@
+package com.hibernatetest;
+
+import com.tables.Customer;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Customer customer = new Customer();
+			System.out.println("dq");
+	}
+
+}
---
+++ TaskOne/src/com/tables/Customer.java
@@ -0,0 +1,34 @@
+package com.tables;
+
+public class Customer {
+	private int personelid;
+	private String personelAdi,personelSoyadi;
+	public int getPersonelid() {
+		return personelid;
+	}
+	
+	
+	public void setPersonelid(int personelid) {
+		this.personelid = personelid;
+	}
+	
+	
+	public String getPersonelAdi() {
+		return personelAdi;
+	}
+	
+	
+	public void setPersonelAdi(String personelAdi) {
+		this.personelAdi = personelAdi;
+	}
+	
+	
+	public String getPersonelSoyadi() {
+		return personelSoyadi;
+	}
+	
+	
+	public void setPersonelSoyadi(String personelSoyadi) {
+		this.personelSoyadi = personelSoyadi;
+	}
+}
